35f5b5287829bc5ab646dc3822dea27e8c712c06
Anthony Ishkan <anthony.ishkan@gmail.com>
2018-04-18 09:23:15 +1000

    Add missing status, fix existing.

--- migrations/0001_base inserts.sql
+++ migrations/0001_base inserts.sql
@@ -1,6 +1,7 @@
-USE shakingdog;
+USE shakingdog;INSERT INTO ailmentstatus (status) VALUES ('CarrierByParentage');
 INSERT INTO ailmentstatus (status) VALUES ('Unknown');
 INSERT INTO ailmentstatus (status) VALUES ('Clear');
 INSERT INTO ailmentstatus (status) VALUES ('ClearByProgeny');
 INSERT INTO ailmentstatus (status) VALUES ('Carrier');
 INSERT INTO ailmentstatus (status) VALUES ('Affected');
+INSERT INTO ailmentstatus (status) VALUES ('CarrierByParentage');
