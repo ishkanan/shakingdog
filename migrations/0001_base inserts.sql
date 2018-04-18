b3bc59b557240edca188a6fdfe6b36d74b56273d
Anthony Ishkan <anthony.ishkan@gmail.com>
2018-04-18 16:53:07 +1000

    Fix duplicate status in SQL.

--- migrations/0001_base inserts.sql
+++ migrations/0001_base inserts.sql
@@ -1,7 +1,7 @@
-USE shakingdog;INSERT INTO ailmentstatus (status) VALUES ('CarrierByParentage');
-INSERT INTO ailmentstatus (status) VALUES ('Unknown');
-INSERT INTO ailmentstatus (status) VALUES ('Clear');
-INSERT INTO ailmentstatus (status) VALUES ('ClearByProgeny');
-INSERT INTO ailmentstatus (status) VALUES ('Carrier');
+USE shakingdog;
 INSERT INTO ailmentstatus (status) VALUES ('Affected');
-INSERT INTO ailmentstatus (status) VALUES ('CarrierByParentage');
+INSERT INTO ailmentstatus (status) VALUES ('Carrier');
+INSERT INTO ailmentstatus (status) VALUES ('CarrierByProgeny');
+INSERT INTO ailmentstatus (status) VALUES ('Clear');
+INSERT INTO ailmentstatus (status) VALUES ('ClearByParentage');
+INSERT INTO ailmentstatus (status) VALUES ('Unknown');
